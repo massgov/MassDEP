135813e64bfa8069001c27c0422ffd15a5d8d9b9
Tsuyoshi Yano <tsuyoshi@tsuyoshi-yano.com>
2018-03-28 16:28:59 -0400

    Add polyfill for array.find
    Add npm package array.prototype.find to support IE 11.  Run build.

--- brp/dwp/pws-documents-search/src/index.js
+++ brp/dwp/pws-documents-search/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'array.prototype.find';
 import './assets/mayflower-artifacts/css/index-generated.css';
 import './assets/mayflower-artifacts/css/base-theme-generated.css';
 import './assets/css/app.css';
