31c53a5a66ec595a254432a9db2149f779b38ff9
Tsuyoshi Yano <tsuyoshi@tsuyoshi-yano.com>
2018-03-28 17:25:31 -0400

    Install babel-polyfill node package
    Replace array.prototype.find package with babel-polyfill package.
    Run build.

--- brp/dwp/pws-documents-search/src/index.js
+++ brp/dwp/pws-documents-search/src/index.js
@@ -1,10 +1,12 @@
+import 'babel-polyfill';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './assets/mayflower-artifacts/css/index-generated.css';
 import './assets/mayflower-artifacts/css/base-theme-generated.css';
 import './assets/css/app.css';
-import App from './App';
 
-require('array.prototype.find');
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
