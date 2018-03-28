db9a07ca670c72f071eb2ee690083b91f3947e70
Tsuyoshi Yano <tsuyoshi@tsuyoshi-yano.com>
2018-03-28 17:01:32 -0400

    Modify import to require for array.prototype.find

--- brp/dwp/pws-documents-search/src/index.js
+++ brp/dwp/pws-documents-search/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'array.prototype.find';
 import './assets/mayflower-artifacts/css/index-generated.css';
 import './assets/mayflower-artifacts/css/base-theme-generated.css';
 import './assets/css/app.css';
 import App from './App';
+
+require('array.prototype.find');
 
 ReactDOM.render(<App />, document.getElementById('root'));
