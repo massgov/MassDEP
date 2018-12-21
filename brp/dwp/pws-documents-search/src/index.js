99ebc61e913bd2d29d155a8f406db40ad01c82ed
Tsuyoshi Yano <tsuyoshi@tsuyoshi-yano.com>
2018-12-21 11:46:15 -0500

    Update to mayflower-artifacts v8.14.1

--- brp/dwp/pws-documents-search/src/index.js
+++ brp/dwp/pws-documents-search/src/index.js
@@ -5,7 +5,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './assets/mayflower-artifacts/css/index-generated.css';
-import './assets/mayflower-artifacts/css/base-theme-generated.css';
 import './assets/css/app.css';
 
 import App from './App';
