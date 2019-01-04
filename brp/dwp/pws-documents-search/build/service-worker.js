f0ca6780a87615a2e65ab5c51fe9ea5eab4d6b70
Tsuyoshi Yano <tsuyoshi@tsuyoshi-yano.com>
2019-01-04 17:29:30 -0500

    Update to React v16.7.0
    Update to react@16.7.0 and react-dom@16.7.0.

--- brp/dwp/pws-documents-search/build/service-worker.js
+++ brp/dwp/pws-documents-search/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MassDEP/brp/dwp/pws-documents-search/build/precache-manifest.f4622275d0ad164da3dc6ca2a2a2680f.js"
+  "/MassDEP/brp/dwp/pws-documents-search/build/precache-manifest.d724ec929d532408cfbbf71f83f8ec71.js"
 );
 
 workbox.clientsClaim();
