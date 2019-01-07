aaeae70fb5bc57885dde931cf0d64305ab8818cf
Tsuyoshi Yano <tsuyoshi@tsuyoshi-yano.com>
2019-01-07 14:17:27 -0500

    Remove build folder from .gitignore

---
+++ employee-lookup/build/precache-manifest.4b487614454f706d975f35db1cd971b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca03358e741c3d8986c2dac36a7e469a",
+    "url": "/MassDEP/employee-lookup/build/static/media/stateseal.ca03358e.png"
+  },
+  {
+    "revision": "fbeda59ef4950499945fa94808d0124a",
+    "url": "/MassDEP/employee-lookup/build/static/media/input-error.fbeda59e.svg"
+  },
+  {
+    "revision": "74c19a398a83605e6e90d69140356b0a",
+    "url": "/MassDEP/employee-lookup/build/static/media/date-picker.74c19a39.svg"
+  },
+  {
+    "revision": "52ee31e9a67b3d6bd508",
+    "url": "/MassDEP/employee-lookup/build/static/js/runtime~main.52ee31e9.js"
+  },
+  {
+    "revision": "7acda30e0a35dd3490f4",
+    "url": "/MassDEP/employee-lookup/build/static/js/main.7acda30e.chunk.js"
+  },
+  {
+    "revision": "0176f2ca9bd819792a08",
+    "url": "/MassDEP/employee-lookup/build/static/js/1.0176f2ca.chunk.js"
+  },
+  {
+    "revision": "7acda30e0a35dd3490f4",
+    "url": "/MassDEP/employee-lookup/build/static/css/main.a5395677.chunk.css"
+  },
+  {
+    "revision": "0176f2ca9bd819792a08",
+    "url": "/MassDEP/employee-lookup/build/static/css/1.3763f938.chunk.css"
+  },
+  {
+    "revision": "f1ed928c6fbafc9df9c4f86f99ffcb4f",
+    "url": "/MassDEP/employee-lookup/build/index.html"
+  }
+];
