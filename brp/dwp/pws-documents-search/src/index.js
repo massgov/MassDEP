751c2a87b6bfbde3aa1ceaf11900b59f4903cada
Tsuyoshi Yano <tsuyoshi.yano@gmail.com>
2024-10-11 13:00:30 -0400

    Remove ReactDOM import

--- brp/dwp/pws-documents-search/src/index.js
+++ brp/dwp/pws-documents-search/src/index.js
@@ -3,11 +3,13 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import './assets/mayflower-artifacts/assets/css/index-generated.css';
 import './assets/css/app.css';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
