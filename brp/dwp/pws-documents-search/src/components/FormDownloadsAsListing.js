da5d8c0a12f31c7689ed2fbc981ee5d6e3e90b51
Tsuyoshi Yano <tsuyoshi.yano@gmail.com>
2025-12-06 15:48:26 -0500

    Fix a11y issues. Add new report types.  Fix eslint issues.

--- brp/dwp/pws-documents-search/src/components/FormDownloadsAsListing.js
+++ brp/dwp/pws-documents-search/src/components/FormDownloadsAsListing.js
@@ -1,7 +1,6 @@
 /** src/components/FormDownloadsAsListing.js */
 
 import React from 'react'
-import FileDisplayHelper from '../helpers/FileDisplayHelper'
 import PropTypes from 'prop-types'
 
 /**
@@ -10,7 +9,7 @@ import PropTypes from 'prop-types'
  */
 class FormDownloadsAsListing extends React.Component {
   render () {
-    const files = this.props.documents
+    const docs = this.props.documents
 
     if (this.props.documents.length === 0) {
       return (
@@ -22,37 +21,46 @@ class FormDownloadsAsListing extends React.Component {
     }
     return (
       <section className='ma__form-downloads ma__form-downloads--listing'>
-        <div className='ma__form-downloads__links'>
-          <h3 className='ma__comp-heading'>Documents for Download</h3>
-          {files.map((file, idx) =>
-            <div key={file.type + file.ftype + idx} className='ma__download-link '>
-              <div className='ma__download-link__icon'>
-                {FileDisplayHelper.getSvgIcon((file.ftype))}
-              </div>
-              <div className='ma__download-link__title'>
-                <a className='ma__download-link__file-link' href={file.url} target='_blank' rel='noopener noreferrer'>
-                  <span className='visually-hidden'>
-                    Open {file.type.toUpperCase()} file, {file.fsize}, for
-                  </span>
-                  {this.props.fileTypeMap[file.type]} {(file.year ? ' (' + file.year + ')' : '')}
-                </a>
-                <span className='ma__download-link__file-spec'>
-                  ({file.ftype.toUpperCase()} {file.fsize})
-                </span>
-              </div>
-              <div className='ma__download-link__description'>
-                <section className='ma__rich-text js-ma-rich-text'>
+        <h2 className='ma__comp-heading'>Documents Available for Download</h2>
+        <ul className='downloads-list'>
+          {docs.map((docType, idx) => (
+            docType.files.length > 0
+              ? (
+                <li key={docType.type + idx} className='downloads-list__category'>
+                  <h3 className='ma__comp-heading ma__comp-heading--yellow'>{this.props.fileTypeMap[docType.type]}</h3>
                   <p>
-                    {FileDisplayHelper.getFileDescription(file.type).description}&nbsp;
+                    {docType.type === 'asr' &&
+                      <span>The <a href='https://www.mass.gov/service-details/public-water-supplier-document-search-information' target='_blank' rel='noopener noreferrer'>Annual Statistical Report</a> is an annual report submitted by the PWS capturing basic data such as a PWS&apos; inventory, staffing and contact information, pumping volumes, etc..</span>}
+                    {docType.type === 'cms' &&
+                      <span>The <a href='https://www.mass.gov/service-details/public-water-supplier-document-search-information' target='_blank' rel='noopener noreferrer'>Compliance Monitoring Schedule</a> details the monitoring requirements of the PWS for the current 9-year compliance cycle.</span>}
+                    {docType.type === 'lcr' &&
+                      <span>The <a href='https://www.mass.gov/service-details/public-water-supplier-document-search-information' target='_blank' rel='noopener noreferrer'>Lead and Copper Rule (LCR) Approved Sampling Sites Plan</a> lists all approved primary and alternate sampling locations for lead and copper.</span>}
+                    {docType.type === 'ccr' &&
+                      <span>The <a href='https://www.mass.gov/service-details/public-water-supplier-document-search-information' target='_blank' rel='noopener noreferrer'>Consumer Confidence Report</a> provides information on how your system complies with the drinking water contaminant monitoring requirements during the reporting year.</span>}
+                    {docType.type === 'wqr' &&
+                      <span>The <a href='https://www.mass.gov/service-details/public-water-supplier-document-search-information' target='_blank' rel='noopener noreferrer'>Water Quality Report for Non-Community Systems</a> provides information on how your system complies with the drinking water contaminant monitoring requirements during the reporting year.</span>}
+                    {docType.type === 'cert' &&
+                      <span>The <a href='https://www.mass.gov/service-details/public-water-supplier-document-search-information' target='_blank' rel='noopener noreferrer'>Certificate of Registration</a> indicates that the system is registered with the state of Massachusetts and must comply with 310 CMR 22.00.</span>}
+                    {docType.type === 'sli' &&
+                      <span>The <a href='https://www.mass.gov/service-details/public-water-supplier-document-search-information'>Service Line Inventory</a> is a comprehensive inventory of all service lines within the PWS&apos; distribution system.</span>}
                   </p>
-                  <a className='ma__content-link' href={FileDisplayHelper.getFileDescription(file.type).link} title='' target='_blank' rel='noopener noreferrer'>
-                    <span>More information</span>
-                  </a>
-                </section>
-              </div>
-            </div>
-          )}
-        </div>
+                  <section className='ma__form-downloads__section'>
+                    <h4>Available Downloads:</h4>
+                    <ul className='ma__form-downloads__downloads-list'>
+                      {docType.files.map((file, idx) =>
+                        <li key={docType.type + file.year + idx} className='ma__download-item'>
+                          <a className='ma__download-item__file-link' href={file.url} target='_blank' rel='noopener noreferrer'>
+                            {file.year}
+                          </a>
+                        </li>
+                      )}
+                    </ul>
+                  </section>
+                </li>
+                )
+              : null
+          ))}
+        </ul>
       </section>
     )
   }
